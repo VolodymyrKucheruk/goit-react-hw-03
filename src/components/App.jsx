578cdf776fbb5da3d0c0295bb4dd6179e48c8300
VolodymyrKucheruk <voloda.2014a@gmail.com>
2024-01-28 22:37:02 +0100

    andHomeWork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,61 @@
 import css from "./App.module.css";
 import "modern-normalize";
+import { FcContacts } from "react-icons/fc";
 import { useState, useEffect } from "react";
+import { ContactList } from "./ContactList/ContactList";
+import { SearchBox } from "./SearchBox/SearchBox";
+import { ContactForm } from "./ContactForm/ContactForm";
 
-import { Description } from "./Description/Description";
-import { Options } from "./Options/Options";
-import { Feedback } from "./Feedback/Feedback";
-import { Notification } from "./Notification/Notification";
-
-const stats = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 export const App = () => {
-  const getLocalStorageData = () => {
-    const savedFeedback = window.localStorage.getItem("my-stats");
-    if (savedFeedback !== null) {
-      return JSON.parse(savedFeedback);
-    }
-    return stats;
-  };
-  const [counts, setCounts] = useState(getLocalStorageData);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("saved-contacts"));
+    return savedContacts ? savedContacts.contacts : initialContacts;
+  });
+
+  const [filter, setFilter] = useState("");
+
+  function addContact(newContact) {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  }
+
+  function deleteContact(contactId) {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    );
+  }
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   useEffect(() => {
-    window.localStorage.setItem("my-stats", JSON.stringify(counts));
-  }, [counts]);
+    localStorage.setItem("saved-contacts", JSON.stringify({ contacts }));
+  }, [contacts]);
 
-  const handleCounts = (value) => {
-    setCounts((prevCounts) => ({
-      ...prevCounts,
-      [value]: prevCounts[value] + 1,
-    }));
-  };
-  const resetLocalStorageItem = () => {
-    window.localStorage.removeItem("my-stats");
-  };
-  const resetCounts  = () => {
-    setCounts({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-  let totalFeedback = counts.good + counts.neutral + counts.bad;
-  let percentagePositiveCounts = Math.round(
-    ((counts.good + counts.neutral) / totalFeedback) * 100
-  );
   return (
     <>
-      <Description />
-      <div className={css.wrapperFeedback}>
-        <Options
-          onUpdate={handleCounts}
-          reset={resetCounts}
-          feedbacks={totalFeedback}
-        />
-        {totalFeedback ? (
-          <Feedback
-            value={counts}
-            feedbacks={totalFeedback}
-            reset={resetLocalStorageItem}
-            positiveClicks={percentagePositiveCounts}
-          />
-        ) : (
-          <Notification />
-        )}
+      <div className={css.deviceHeader}>
+        <div className={css.deviceSensors}></div>
       </div>
+
+      <h1 className={css.title}>
+        Phonebook
+        <FcContacts className={css.bookitem} />
+      </h1>
+      <ContactForm value={filteredContacts} onAdd={addContact} />
+      <SearchBox value={filter} onChange={setFilter} />
+      <ContactList
+        value={filteredContacts}
+        onDelete={deleteContact}
+        onAdd={addContact}
+      />
     </>
   );
 };
--- src/components/Notification/Notification.jsx
+++
@@ -1,5 +0,0 @@
-import css from "./Notification.module.css"
-
-export const Notification = () => {
-    return <p className={css.feedback}>No feedbeck yet😭</p>
-}
